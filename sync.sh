c430b7d38e0bdc44377b52c6b1597ac0092948e3
Shane MacPhillamy <shane@blinkmobile.com.au>
2015-04-13 13:10:09 +1000

    Capture schemas from http://iglu.blinkmobile.co

--- sync.sh
+++ sync.sh
@@ -1,1 +1,3 @@
 aws s3 sync schemas/ s3://iglu.blinkmobile.co/schemas/ --region us-west-2 --exclude "*~" --recursive
+aws s3 sync schemas/ s3://au-com-blinkmobile-analytics-ap-southeast-2/schemas/ --region ap-southeast-2 --exclude "*~" --recursive
+aws s3 sync ../unifiedlogging/snowplow/4-storage/redshift-storage/jsonpaths/ s3://au-com-blinkmobile-analytics-ap-southeast-2/jsonpaths/ --region ap-southeast-2 --exclude "*~" --recursive
